b2d7ddd21aa77c2fe435619bd9bcfb28c2575555
Frank Adams <fma7308@gmail.com>
2023-09-02 18:21:07 -0400

    Eliminated usage of InnerHTML property

--- js/app.js
+++ js/app.js
@@ -7,30 +7,55 @@ async function listRepos() {
     // TODO: Add error checking for failed FETCH
 
     // Build the output table
-    const tBody = document.getElementById("tBody");
-    const tableTitle = document.getElementById("tableTitle");
 
-    tableTitle.innerHTML = `Public Repositores for ${gitUser}` // Add the table title
+    const tableTitle = document.getElementById('tableTitle');
+    const tBody = document.getElementById('tBody');
+
+    tableTitle.innerText = `Public Repositores for ${gitUser}` // Add the table title
 
     for (let i in repos) {
         // Create a new row for each element in the object
-        let row = document.createElement('tr');
+
 
         // If no description was given, add a note
         if (repos[i].description === null) {
-            repos[i].description = "No description has been entered for this repository."
+            repos[i].description = 'No description has been entered for this repository.'
         }
 
-        // Build each row
-        row.innerHTML = (`<tr>
-                            <td>${repos[i].name}</td>
-                            <td>${repos[i].description}</td>
-                            <td><a href="${repos[i].html_url}" target="_new">${repos[i].html_url}</a></td>
-                          </tr>`
-        )
+        // Insert a new row
+        let newRow = tBody.insertRow(i);
+
+        // Insert the cells
+        let firstCell = newRow.insertCell(0);
+        let secondCell = newRow.insertCell(1);
+        let thirdCell = newRow.insertCell(2);
+
+        // Add the object data to the cells
+        firstCell.innerText = repos[i].name;
+        secondCell.innerText = repos[i].description;
+        thirdCell.insertAdjacentHTML('afterbegin', `<a href="${repos[i].html_url}" target="_blank">${repos[i].html_url}</a>`);
+
+
+        // Build each cell
+        // newRow.appendChild(newCell);
+        // newCell.innerText = repos[i].name;
+
+
+        // newRow.appendChild(newCell);
+        // newCell.innerText = repos[i].description;
+
+        // newRow.appendChild(newCell);
+        // newCell.innerText = repos[i].html_url;
+
+        // row.innerHTML = (`<tr>
+        //                     <td>${repos[i].name}</td>
+        //                     <td>${repos[i].description}</td>
+        //                     <td><a href="${repos[i].html_url}" target="_new">${repos[i].html_url}</a></td>
+        //                   </tr>`
+        // )
 
         // Add the row to the table
-        tBody.appendChild(row);
+        // tBody.appendChild(row);
     }
 }
 
