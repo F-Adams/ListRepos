d25b2c6b10d92f3182c894638db790ef0b32da1e
Frank Adams <fma7308@gmail.com>
2023-09-13 00:36:30 -0400

    Built output HTML with JS instead of hard coding

--- js/app.js
+++ js/app.js
@@ -6,18 +6,62 @@ async function listRepos(gitUser) {
     const gitUrl = `https://api.github.com/users/${gitUser}/repos`; // URL for API Call
     const response = await fetch(gitUrl);   // Fetch the repository list
     const repos = await response.json();    // Wait for the JSON response
-    let numRepos = 0;
+
+    // Finish timing the function
+    let timeTaken = Date.now() - start;
 
     // TODO: Add error checking for failed FETCH
     // TODO: Handle result pagination
-    // TODO: Build full table programmatically
 
-    // Build the output table
-    const tableTitle = document.getElementById('tableTitle');
-    const tBody = document.getElementById('tBody');
-    const result = document.getElementById('result');
+    // Display the search results
+    // Get the SECTION element, and clear it of any previously generated HTML
+    const resultSection = document.getElementById('resultTable');
+    resultSection.innerText = '';
 
-    tableTitle.innerText = `Public Repositories for ${gitUser}` // Add the table title
+    // Create the table and add it to the result section
+    const tbl = document.createElement('table');
+    tbl.classList.add('resultTable');
+    resultSection.appendChild(tbl);
+
+    // Add a caption to the table
+    const tblTitle = document.createElement('caption');
+    tblTitle.classList.add('tableCaption');
+    tblTitle.innerText = `Public Repositories for ${gitUser}`
+    tbl.appendChild(tblTitle);
+
+    // Build the THEAD 
+    const tblHead = document.createElement('thead');
+    tbl.appendChild(tblHead);
+
+    // Add a row to the THEAD (TR element)
+    const tblHeadRow = document.createElement('tr');
+
+    // Add the header cells to the row (TH element)
+    let tblHeadCell = document.createElement('th');
+    tblHeadCell.innerText = 'Repository Name';
+    tblHeadRow.appendChild(tblHeadCell);
+
+    tblHeadCell = document.createElement('th');
+    tblHeadCell.innerText = 'Description';
+    tblHeadRow.appendChild(tblHeadCell);
+
+    tblHeadCell = document.createElement('th');
+    tblHeadCell.innerText = 'URL';
+    tblHeadRow.appendChild(tblHeadCell);
+
+    // Append the row of TH elements to the THEAD
+    tblHead.appendChild(tblHeadRow);
+
+    // Append the THEAD to the table
+    tbl.appendChild(tblHead);
+
+    // Build the TBODY (where the search results are displayed)
+    const tblBody = document.createElement('tbody');
+    tbl.appendChild(tblBody);
+
+    // Build and display the results
+    // Initialize a variable to count the repos
+    let numRepos = 0;
 
     for (let i in repos) {
         // If no description was given, add a note
@@ -27,33 +71,70 @@ async function listRepos(gitUser) {
 
         // Create the hyperlink for the repository URL
         let htmlLink = document.createElement('a');
-        let linkText = document.createTextNode(`${repos[i].html_url}`);
-        htmlLink.appendChild(linkText);
+        htmlLink.innerText = `${repos[i].html_url}`
         htmlLink.target = '_blank'; // Make it open in a new window/tab
         htmlLink.href = `${repos[i].html_url}`;
 
-        // Insert a new row
-        let newRow = tBody.insertRow(i);
+        // Add a row to the TBODY
+        const tblBodyRow = document.createElement('tr');
+        tblBody.appendChild(tblBodyRow);
 
-        // Insert the cells
-        let firstCell = newRow.insertCell(0);
-        let secondCell = newRow.insertCell(1);
-        let thirdCell = newRow.insertCell(2);
+        // Add the result cells to the row (TD element)
+        let tblBodyCell = document.createElement('td');
+        tblBodyCell.innerText = `${repos[i].name}`
+        tblBodyRow.appendChild(tblBodyCell);
 
-        // Add the object data to the cells
-        firstCell.innerText = repos[i].name;
-        secondCell.innerText = repos[i].description;
-        thirdCell.appendChild(htmlLink);
+        tblBodyCell = document.createElement('td');
+        tblBodyCell.innerText = `${repos[i].description}`
+        tblBodyRow.appendChild(tblBodyCell);
+
+        tblBodyCell = document.createElement('td');
+        tblBodyCell.appendChild(htmlLink);
+        tblBodyRow.appendChild(tblBodyCell);
+
+        // Increment the repo counter
         numRepos++;
     }
 
-    // Finish timing the function
-    let timeTaken = Date.now() - start;
+    // Build the TFOOT
+    const tblFooter = document.createElement('tfoot');
+    tbl.appendChild(tblFooter);
 
-    // Write the timing results
-    result.innerText = 'Found ' + numRepos + ' repositories in ' + timeTaken / 1000 + ' seconds';
-    console.log('Total time taken : ' + timeTaken + ' milliseconds for ' + numRepos + ' repos');
+    const tblFooterRow = document.createElement('tr');
+    tblFooter.appendChild(tblFooterRow);
+
+    const tblFooterCell = document.createElement('td');
+    tblFooterCell.classList.add('result');
+    tblFooterCell.colSpan = 3;
+    tblFooterCell.innerText = `Found ${numRepos} repositories in ${timeTaken / 1000} seconds`;
+    tblFooterRow.appendChild(tblFooterCell);
+
+    console.clear();
+    console.log(`Total time taken: ${timeTaken} milliseconds for ${numRepos} repos for GitHub user: ${gitUser}`);
+    console.log(tbl);
 }
 
-// TODO: Build a form to allow the user to input a GitHub username
-listRepos('F-Adams');
+// Get the form element
+const searchForm = document.getElementById('searchForm');
+
+// Add an event listener to the form, listening for SUBMIT
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get the user submitted GitHub username to search for
+    let searchTerm = document.getElementById('gitUser');
+
+    // Handle the input
+    if (searchTerm.value === '') {
+        // If no username was submitted, let's use a hard coded default (This would not happen in production)
+        // TODO: Implement proper input validation and error handling
+        gitUser = 'F-Adams';
+    } else {
+        gitUser = searchTerm.value;
+    }
+
+    // Clear the form input
+    searchTerm.value = '';
+
+    listRepos(gitUser);
+});
